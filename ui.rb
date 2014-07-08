6f7913fd3dc9214ed35a656b24101b2bebc708d2
blasterbb <danvpop1@gmail.com>
2014-07-08 16:47:41 +0300

    Dan

---
+++ ui.rb
@@ -0,0 +1,44 @@
+load 'DryClean.rb'
+
+class Ui
+  def runUi
+    print "Open="
+    @openHour=gets.chomp.to_i
+    #puts ''
+    print "Close="
+    @closeHour=gets.chomp.to_i
+    
+    print "Pickup time(hours)="
+    @pickupTime=gets.chomp.to_i
+    print "Pickup time(minutes)="
+    @pickupTimeMin=gets.chomp.to_i
+    
+    @pick=@pickupTime*60 + @pickupTimeMin
+    
+
+    if self.validation
+      o=Order.new
+      o.init(@openHour,@closeHour,@pick)
+      o.getTime
+      print "time is:",o.calculate((o.getTime*60+@pick)<@closeHour*60)
+    end
+
+  end
+
+  def validation
+    v=Validator.new
+    if !v.openCloseValid(@openHour,@closeHour)
+      print "Error! You can't open after closing hour!!"
+    elsif !v.pickupValid(@pick)
+      print "Error! check pickup time"
+    elsif v.isClosed(@closeHour,Time.new.hour)
+      print "Is closed"
+    else true
+    end
+  end
+end
+
+
+
+ui=Ui.new
+ui.runUi
