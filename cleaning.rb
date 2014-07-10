de7d41ed5508b2e1b6d3a4a221959fb105678f51
Iulia_Ionita <ionita_iuliadiana@yahoo.com>
2014-07-10 14:11:19 +0300

    Final modifications

--- cleaning.rb
+++ cleaning.rb
@@ -1,72 +1,59 @@
 
 class DryCleaning
-   
-def userInput
-  puts "Opening Time:" 
-  @open_time = gets.chomp.to_i * 60 * 60
-  puts "Closing Time:" 
-  @close_time = gets.chomp.to_i * 60 * 60
-  puts "Give wash time in minutes:"
-  @wash_time = gets.chomp.to_i * 60
-  while @wash_time > 120 * 60
-    puts "A washing cycle can't be longer than 2 hours! Please enter the correct time!!" 
-    @wash_time = gets.chomp.to_i * 60
-  end
-end
 
-def transformCurrentTimeIntoSeconds
+attr_accessor :open_time, :close_time, :wash_time
+
+def initialize(open,close,wash)
+  @open_time = open
+  @close_time = close
+  @wash_time = wash
+end
+   
+def transform_current_time_into_seconds
   hours = Time.new.hour
   minutes = Time.new.min
   seconds = Time.new.sec
   @curr_time_in_sec = hours * 60 * 60 + minutes * 60 + seconds
 end
 
-def differenceDay
+def difference_day
   difference = 24 * 60 * 60 - @close_time + @open_time
 end
 
-def differenceWeekend
+def difference_weekend
   difference = 24 * 3 * 60 * 60 - @close_time + @open_time
 end
 
-def checkWashingForToday
-  if transformCurrentTimeIntoSeconds >= @open_time && transformCurrentTimeIntoSeconds <= @close_time - @wash_time
+def check_washing_for_today
+  if transform_current_time_into_seconds >= @open_time && transform_current_time_into_seconds <= @close_time - @wash_time
     finish_time = Time.now + @wash_time
     puts "You can pick up your clothes #{finish_time.ctime}"
   end
 end
 
-def checkWashingForNextDay
-  if Time.new.wday >=1 && Time.new.wday <=4 && transformCurrentTimeIntoSeconds > @close_time - @wash_time && transformCurrentTimeIntoSeconds < @close_time
-    finish_time = Time.now + @wash_time + differenceDay
+def check_washing_for_next_day
+  if Time.new.wday >=1 && Time.new.wday <=4 && transform_current_time_into_seconds > @close_time - @wash_time && transform_current_time_into_seconds < @close_time
+    finish_time = Time.now + @wash_time + difference_day
     puts "You can pick up your clothes #{finish_time.ctime}"
   end
 end
 
-def checkWashingForNextWeek
-  if Time.new.wday == 5 && transformCurrentTimeIntoSeconds > @close_time - @wash_time && transformCurrentTimeIntoSeconds < @close_time
-    finish_time = Time.now + @wash_time + differenceWeekend
+def check_washing_for_next_week
+  if Time.new.wday == 5 && transform_current_time_into_seconds > @close_time - @wash_time && transform_current_time_into_seconds < @close_time
+    finish_time = Time.now + @wash_time + difference_weekend
     puts "You can pick up your clothes #{finish_time.ctime}"
   end
 end
 
-def verifyIfOpen
+def verify_if_open
   if Time.new.wday == 0 || Time.new.wday == 6
     puts "Sorry, we're Closed! It's weekend!"
-  elsif transformCurrentTimeIntoSeconds > @close_time || transformCurrentTimeIntoSeconds < @open_time
+  elsif transform_current_time_into_seconds > @close_time || transform_current_time_into_seconds < @open_time
     puts "Sorry, we're Closed!"
   else
     puts "Clothes were brought in #{Time.now.ctime}"
   end
 end
 
-def pickUpTime
-  userInput
-  verifyIfOpen
-  checkWashingForToday
-  checkWashingForNextDay
-  checkWashingForNextWeek
-end  
-  
 end
 
--- main.rb
+++ main.rb
@@ -1,4 +1,18 @@
 require_relative "cleaning.rb"
 
-dry_cleaner = DryCleaning.new
-puts dry_cleaner.pickUpTime
+puts "Opening Time:" 
+@open_time = gets.chomp.to_i * 60 * 60
+puts "Closing Time:" 
+@close_time = gets.chomp.to_i * 60 * 60
+puts "Give wash time in minutes:"
+@wash_time = gets.chomp.to_i * 60
+while @wash_time > 120 * 60
+  puts "A washing cycle can't be longer than 2 hours! Please enter the correct time!!" 
+  @wash_time = gets.chomp.to_i * 60
+end
+
+dry_cleaner = DryCleaning.new(@open_time, @close_time, @wash_time)
+puts dry_cleaner.verify_if_open
+puts dry_cleaner.check_washing_for_today
+puts dry_cleaner.check_washing_for_next_day
+puts dry_cleaner.check_washing_for_next_week
