720826fc8ed1cc7737701806c5568a897885290f
Catalin94 <rock_funn94@yahoo.com>
2014-07-10 13:43:25 +0300

    Add the free day

--- curata.rb
+++ curata.rb
@@ -1,34 +1,46 @@
-$opening_hour=8
-$closing_hour=20
 
 
 
-class Spalatorie
+class Cleaning
 	attr_accessor:durata
 	attr_accessor:time
 	attr_accessor:returntime
-	def initialize(time_necesar,time_comanda)
-		@durata=time_necesar*60
-		@time=time_comanda
-		@returntime=@time+@durata
+	attr_accessor:opening_hour
+	attr_accessor:closing_hour
+	attr_accessor:free_time
+
+
+	def initialize(necesary_time,opening_hour=8,closing_hour=16)
+		@opening_hour=opening_hour
+		@closing_hour=closing_hour
+		@lenght=necesary_time*60
+		@time=Time.mktime(2016,9,29,14,0,0)
+		@returntime=@time+@lenght
+		@free_time=Time.mktime(2016,9,30,8,0,0)
 	end
 
 
+	def opening_hour=(hour)
+		@opening_hour=hour
+	end
+	def closing_hour=(hour)
+		@closing_hour=(hour)
+	end
+		
 =begin
 	verifica urmatoarele lucruri:
 		1.daca spalatoria este deschisa
 		2.daca se poate realiza job-ul in ziua curenta
 		3.daca ziua curenta nu este vineri
 =end
-	
-	def check_time()
-	
-		unless check_validity==false
-			if @returntime.friday? and @returntime.hour >= $closing_hour
-				weekend(@returntime)
 
-			elsif @returntime.hour >= $closing_hour or @returntime.hour<$opening_hour
-				tomorrow(@returntime)
+	def check_time()
+		unless check_validity==false
+			if @returntime.friday? and @returntime.hour >= @closing_hour
+				computes(3)
+
+			elsif @returntime.hour >= @closing_hour or @returntime.hour<@opening_hour
+				computes(1)
 			else
 				print_current_day(@returntime)
 			end
@@ -37,53 +49,53 @@ class Spalatorie
 
 
 
-
-
-	
-
 private
 def check_validity()
-	if $closing_hour<$opening_hour
-		puts "The opening and closing hour are not fine defined"
-		return false
-	end
-=begin
-	if @@durata>7200
-		puts "The maximum time for cleaning is 2 hour(120 minutes)"
-		return false
-	end
-=end
-	if @time.hour>=$closing_hour or @time.hour<$opening_hour
-	  puts "Please come to put the command in the orar interval 8 am - 20 pm"
-	  return false
+	unless @closing_hour==0
+		if @time.day===@free_time.day
+			puts "Today is a free day! It's closed!"
+			return false
+		end
+
+		if @closing_hour<@opening_hour
+			puts "The opening and closing hour are not fine defined"
+			return false
+		end
+		if @time.hour<@opening_hour or @time.hour>@closing_hour
+			puts "Closed"
+			return false
+		end
+		if @lenght>7200
+			puts "The maximum time for cleaning is 2 hour(120 minutes)"
+			return false
+		end
 	end
 end
-
-
-	def weekend(returntime)
-		computes(3)
-	end
-
-	def tomorrow(returnt)
-		computes(1)
-	end
 	
-	
+
+
 	def print_current_day(durata)
 		puts "you can come back at #@returntime"
 	end
 
+
+
+
 	def computes(i)
-		@@dif_h=@returntime.hour-$closing_hour
-		@@dif_min=@returntime.min
-		@@dif_sec=@returntime.sec
+		dif_h=@returntime.hour-@closing_hour
+		puts dif_h
+		dif_min=@returntime.min
+		dif_sec=@returntime.sec
 		@returntime=@returntime+i*24*60*60
-		@returntime=Time.mktime(@returntime.year,@returntime.month,@returntime.day,@@dif_h+8,@@dif_min,@@dif_sec)
-		puts "you can come back at #@returntime"
+		if @returntime.day==@free_time.day and @free_time.friday?
+			@returntime=@returntime+3*24*60*60
+		end
+		if @returntime.day==@free_time.day
+			@returntime=@returntime+1*24*60*60
+		end
+		@returntime=Time.mktime(@returntime.year,@returntime.month,@returntime.day,dif_h+8,dif_min,dif_sec)	
+		puts "you can come back at #{@returntime}"
 	end
 
 end
 
-#the initialize time is in minutes
-spal=Spalatorie.new(110,Time.mktime(2014,7,11,19,0,0))
-spal.check_time()
---
+++ launch.rb
@@ -0,0 +1,3 @@
+load "/home/catalin/Surse_ruby/cleaning/curata.rb"
+cleaner=Cleaning.new(120,8,15)
+cleaner.check_time
